d75668c84f961a06f6f708494aee32b1228408ad
vietnguyen2000 <viet.nguyen.2000@hcmut.edu.vn>
2020-11-15 14:05:22 +0700

    add footer and header

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const sql = require('mysql');
 
-// router.get('/', (req, res) => {
-//     res.render('landing')
-// })
+router.get('/', (req, res) => {
+    res.render('landing')
+})
 
 module.exports = router;
