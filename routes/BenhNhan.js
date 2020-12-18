f7f2c93366371b1140e02280e4175b46e6e4eceb
Nguyễn Văn Biên <>
2020-12-18 11:24:24 +0700

    Thêm chức năng: Xem danh sách xét nghiệm (trong tất cả các lần và lần gần nhất)

--- routes/BenhNhan.js
+++ routes/BenhNhan.js
@@ -30,6 +30,7 @@ router.get('/',Authenticate , (req, res) =>{
   res.render('BenhNhan/index');
 })
 
+// (iii.2,iii.3) Xem danh sách các thuốc (gần nhất và tất cả các lần)
 router.get('/DSThuoc', Authenticate, (req,res)=>{
   res.render('BenhNhan/DSThuoc', {kq: null});
 })
@@ -46,6 +47,25 @@ router.post('/DSThuoc', Authenticate, (req,res)=>{
     res.render('BenhNhan/DSThuoc', {kq: result[0]});
   })
 })
+
+// (iii.4, iii.5). Xem kết quả xét nghiệm (gần nhất và tất cả các lần)
+router.get('/DSXetNghiem', Authenticate, (req,res)=>{
+  res.render('BenhNhan/DSXetNghiem', {kq: null});
+})
+router.post('/DSXetNghiem', Authenticate, (req,res)=>{
+  const {Loai} = req.body;
+  const MaBenhNhan = req.user.MaBenhNhan;
+  if (Loai == "All")
+    var sql = "call DSXetNghiem(?)";
+  else
+    var sql = "call DSXetNghiemGanNhat(?)";
+  mysql.query(sql,[MaBenhNhan],(err,result)=>{
+    if (err) throw err;
+    console.log(result[0]);
+    res.render('BenhNhan/DSXetNghiem', {kq: result[0]});
+  })
+})
+
 
 // TODO: Viết router.get và router.post mỗi chức năng mà đề yêu cầu, vui lòng đọc qua hết các chức năng cần hiện thực và gom nhóm các chức năng lại một cách gọn gàng nhất.
 // ! Có một số chức năng nhỏ nằm trong 1 chức năng lớn, thì có thể gom thành 1 route và nhiều post để thực thi 
